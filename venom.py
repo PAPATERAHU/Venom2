d23623fa0d03c77ab342c10398ef8efbfb0fd597
PAPATERAHU <yt891718@gmail.com>
2024-07-01 10:49:43 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -16,7 +16,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = 'TOKEN'
+TOKEN = '7472311660:AAGei7o5dNK21nlfhxlF-yxCtxy1Wvex2Ug'
 MONGO_URI = 'mongodb+srv://VENOMxCRAZY:CRAZYxVENOM@cluster0.ythilmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true'
 FORWARD_CHANNEL_ID = -1002224570220
 CHANNEL_ID = -1002224570220
